ecbd2842c9fba457482e1b8a3c42161aa143ebf4
Nicaredda <workcaredda1998@gmail.com>
2026-01-12 17:08:03 +0100

    must still make the container immutable

--- script.js
+++ script.js
@@ -5,20 +5,48 @@ const container = document.querySelector(".container");
 // therefore we will create 16 rows. Each of the rows is a single container 
 // that contains 16 others squares
 
-let inputtedSquares = prompt("How many squares do you need?")
+let btn = document.querySelector("button")
+btn.addEventListener("click", inputSquare);
 
-for (let n=0;n<inputtedSquares;n++){
-    const rows = document.createElement("div");
-    rows.className = "rows";
-    container.append(rows);
+function inputSquare () {
+    inputtedSquares = prompt("How many squares do you need?");
+    if inputtedSquares<=100? makerGrid(): alert("insert a number between 1 and 100101"), return;
+    return inputtedSquares;
+}
+
+function makerGrid () {
+    removeGrid();
     for (let n=0;n<inputtedSquares;n++){
-        const squares = document.createElement("div");
-        squares.className = "squares";
+        const rows = document.createElement("div");
+        rows.className = "rows";
+        container.append(rows);
+        for (let n=0;n<inputtedSquares;n++){
+            const squares = document.createElement("div");
+            squares.className = "squares";
 
-        squares.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = "black"
-        })
+            squares.addEventListener("mouseover", (event) => {
+                event.target.style.backgroundColor = "black";
+            })
 
-        rows.append(squares);
+            rows.append(squares);
+        }
     }
 }
+
+
+// Da fixare
+function removeGrid() {
+    const rows = document.querySelectorAll(".rows");
+    rows.forEach((row) => row.remove());
+}
+
+function program () {
+    inputSquare();
+    makerGrid();
+}
+
+
+program();
+
+
+

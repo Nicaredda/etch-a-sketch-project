3dc7ea96ba366933854e0db5bfc5680ab3802d38
nicaredda <workcaredda1998@gmail.com>
2026-01-13 16:39:20 +0100

    rainbow drawing function added, still need fixes

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const body = document.querySelector("body");
-const container = document.querySelector(".container");
+
 
 // the assignment is asking us to make a 16*16 squares grid, 
 // therefore we will create 16 rows. Each of the rows is a single container 
 // that contains 16 others squares
 
-let btn = document.querySelector("button")
-btn.addEventListener("click", inputSquare);
-
-function inputSquare () {
+function inputSquare() {  
     inputtedSquares = prompt("How many squares do you need?");
-    if inputtedSquares<=100? makerGrid(): alert("insert a number between 1 and 100101"), return;
+    if (inputtedSquares<=100){
+        makerGrid()
+    } else {
+        alert("insert a number between 1 and 101") 
+        return;
+    } 
     return inputtedSquares;
 }
 
-function makerGrid () {
+function rainbowPencil(squares) {
+    function randomNumber(){
+        
+    }
+    let rNum = Math.floor(Math.random() * 255);
+    squares.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = `rgb(${rNum},${rNum},${rNum})`;
+    })
+}
+
+
+function blackPencil(squares) {
+    squares.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+    })
+}
+
+function makerGrid() {
     removeGrid();
     for (let n=0;n<inputtedSquares;n++){
         const rows = document.createElement("div");
@@ -23,28 +41,30 @@ function makerGrid () {
         for (let n=0;n<inputtedSquares;n++){
             const squares = document.createElement("div");
             squares.className = "squares";
-
-            squares.addEventListener("mouseover", (event) => {
-                event.target.style.backgroundColor = "black";
-            })
-
+            rainbowPencil(squares);
             rows.append(squares);
         }
     }
 }
 
-
-// Da fixare
 function removeGrid() {
     const rows = document.querySelectorAll(".rows");
     rows.forEach((row) => row.remove());
 }
 
-function program () {
+function program() {
     inputSquare();
     makerGrid();
 }
 
+const btnGrid = document.querySelector("#changeGrid")
+btnGrid.addEventListener("click", inputSquare);
+
+const btnRainbow = document.querySelector("#rainbow")
+btnRainbow.addEventListener("click", rainbowPencil)
+
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
 
 program();
 
